98ceeb13f1b31bd30e6d3c705a994ff888804ab1
nalinraut <rautnalin@gmail.com>
2020-04-18 12:43:27 -0400

    Added progress bar

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -56,25 +56,25 @@ $(document).ready(function() {
   });
 });
 
-// Progress bar
-// var lang = {
-//   "html": "100%",
-//   "css": "90%",
-//   "javascript": "70%",
-//   "php": "55%",
-//   "angular": "65%"
-// };
+//Progress bar
+var lang = {
+  "html": "100%",
+  "css": "90%",
+  "javascript": "70%",
+  "php": "55%",
+  "angular": "65%"
+};
 
-// var multiply = 4;
+var multiply = 4;
 
-// $.each( lang, function( language, pourcent) {
+$.each( lang, function( language, pourcent) {
 
-//   var delay = 700;
+  var delay = 700;
   
-//   setTimeout(function() {
-//     $('#'+language+'-pourcent').html(pourcent);
-//   },delay*multiply);
+  setTimeout(function() {
+    $('#'+language+'-pourcent').html(pourcent);
+  },delay*multiply);
   
-//   multiply++;
+  multiply++;
 
-// });
+});
